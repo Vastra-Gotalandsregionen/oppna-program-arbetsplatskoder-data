ee6c761bd3538ac494bb7c1cf06925f661c1dda6
Patrik Björk <patrik.bjork@codespirit.se>
2019-03-05 08:20:09 +0100

    Fix timestamp to a year ago for Sesam export.

--- core-bc/composites/svc/src/main/java/se/vgregion/arbetsplatskoder/service/SesamLmnDatabaseIntegrationService.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/arbetsplatskoder/service/SesamLmnDatabaseIntegrationService.java
@@ -11,10 +11,11 @@ import se.vgregion.arbetsplatskoder.domain.jpa.migrated.DataExport;
 import se.vgregion.arbetsplatskoder.domain.jpa.migrated.Viewapkforsesamlmn;
 import se.vgregion.arbetsplatskoder.export.repository.DataExportRepository;
 import se.vgregion.arbetsplatskoder.export.repository.ViewApkForSesamLmnRepository;
-import se.vgregion.arbetsplatskoder.repository.Ao3Repository;
 import se.vgregion.arbetsplatskoder.repository.DataRepository;
 
 import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -50,8 +51,8 @@ public class SesamLmnDatabaseIntegrationService {
         LOGGER.info("Deleted all Data in export-db: " + stopWatch.getTotalTimeMillis() + " ms");
 
         stopWatch.start();
-        Timestamp thanThat = new Timestamp(System.currentTimeMillis() - (1000 * 60 * 60 * 24 * 365));
-        List<Data> datas = dataRepository.findAllTillDatumGreaterOrForTheTimeBeing(thanThat);
+        Timestamp aYearAgo = Timestamp.from(ZonedDateTime.now(ZoneId.of("Europe/Stockholm")).minusYears(1).toInstant());
+        List<Data> datas = dataRepository.findAllTillDatumGreaterOrForTheTimeBeing(aYearAgo);
         for (Data data : datas) {
             viewApkForSesamLmnRepository.save(new Viewapkforsesamlmn(data));
             dataExportRepository.save(new DataExport(data));
